524e80910866eeee351ae558e52aa9ccacd8e311
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-02 17:00:20 +0200

    chore: Remove unnecessary code in LeaderCard component

--- src/components/LeaderCard.jsx
+++ src/components/LeaderCard.jsx
@@ -3,15 +3,12 @@ const LeaderCard = ({ leader }) => {
   return (
     <li className="flex flex-wrap justify-between items-baseline gap-4 bg-gray-700 px-4">
       <h2 className="font-semibold text-xl">{leader.name}</h2>
-
-
       <p>
         S: {leader.fights.score || 0} | 
         W: {leader.fights.wins || 0} | 
         L: {leader.fights.losses || 0} | 
         T: {leader.fights.total || 0}
       </p>
-
     </li>
   );
 }
