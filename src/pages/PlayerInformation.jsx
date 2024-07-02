052b45d998f9b069ae7cae95f934d2271a2ac4d8
Anna N <nahid.najafi.h@gmail.com>
2024-07-02 23:44:56 +0200

    changed font, created rotating popup cards and progress chartbar

--- src/pages/PlayerInformation.jsx
+++ src/pages/PlayerInformation.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../components/Card.jsx";
 import PokeInfo from "../components/PokeInfo.jsx";
+import "../animations.css";
+import "../style.css";
 
 const PlayerInformation = () => {
   const [pokeData, setPokeData] = useState([]);
@@ -50,57 +52,55 @@ const PlayerInformation = () => {
   return (
     <main className="bg-gray-100 p-4">
       <div className="container mx-auto p-4">
-      
-          <div className="col-span-2">
-            <Card
-              pokemon={pokeData}
-              loading={loading}
-              infoPokemon={handlePokemonClick}
-            />
+        <div className="col-span-2">
+          <Card
+            pokemon={pokeData}
+            loading={loading}
+            infoPokemon={handlePokemonClick}
+          />
 
-            <div className="flex justify-between mt-4">
-              {prevUrl && (
-                <button
-                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-                  onClick={() => {
-                    setPokeData([]);
-                    setUrl(prevUrl);
-                  }}
-                >
-                  Previous
-                </button>
-              )}
+          <div className="flex justify-between mt-4">
+            {prevUrl && (
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+                onClick={() => {
+                  setPokeData([]);
+                  setUrl(prevUrl);
+                }}
+              >
+                Previous
+              </button>
+            )}
 
-              {nextUrl && (
-                <button
-                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-                  onClick={() => {
-                    setPokeData([]);
-                    setUrl(nextUrl);
-                  }}
-                >
-                  Next
-                </button>
-              )}
-            </div>
+            {nextUrl && (
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+                onClick={() => {
+                  setPokeData([]);
+                  setUrl(nextUrl);
+                }}
+              >
+                Next
+              </button>
+            )}
           </div>
         </div>
+      </div>
 
-        {isModalOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded shadow-lg w-11/12 md:w-1/2 lg:w-1/3 relative">
-              <button
-                className="absolute top-0 right-0 m-4 text-2xl font-bold text-gray-600 hover:text-gray-800"
-                onClick={closeModal}
-              >
-                &times;
-              </button>
-              <PokeInfo data={pokeDex} />
-            </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-6 rounded shadow-lg w-11/12 md:w-1/2 lg:w-1/3 relative rotate-card">
+            <button
+              className="absolute top-0 right-0 m-4 text-2xl font-bold text-gray-600 hover:text-gray-800"
+              onClick={closeModal}
+            >
+              &times;
+            </button>
+            <PokeInfo data={pokeDex} />
           </div>
-        )}
-      </main>
-   
+        </div>
+      )}
+    </main>
   );
 };
 
