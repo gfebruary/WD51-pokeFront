e47dc673a0061f8c1950f59da3b4eef87bce72dc
Anna N <nahid.najafi.h@gmail.com>
2024-07-07 00:18:00 +0200

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,32 @@
 import { Link } from "react-router-dom";
-import homeBg from "../assets/home-bg.jpg";
-import pikachuGif from "../assets/pikachu-gif.gif";
+import pokemon from "../assets/pokemon.jpg";
+import backgroundFoto from "../assets/backgroundFoto.jpg";
 
 const Home = ({ user, appName }) => {
-
   return (
     <main
       className="grid place-content-center bg-cover bg-center"
-      style={{ backgroundImage: `url(${homeBg})` }}>
+      style={{ backgroundImage: `url(${backgroundFoto})` }}
+    >
       <div className="flex flex-col items-center m-2 p-8 bg-white/75 rounded-lg shadow-lg ">
         <img
-          src={pikachuGif}
+          src={pokemon}
           alt="Pikachu"
           className="w-32 h-32 rounded-full mx-auto mb-4 shadow-lg border-4 border-yellow-500"
         />
-        <h1 className="text-5xl font-extrabold text-yellow-500 drop-shadow mb-2 text-balance text-center">
+        <h1 className="text-5xl font-extrabold text-yellow-600 drop-shadow mb-2 text-balance text-center">
           Welcome to {appName}
         </h1>
         <p className="text-xl text-gray-700 mb-2">Enjoy your adventure!</p>
         <p className="text-xl text-gray-700 mb-6">
-          {user ? `${user.name}, welcome back!` : "Sign up or log in to start playing."}
+          {user
+            ? `${user.name}, welcome back!`
+            : "Sign up or log in to start playing."}
         </p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {user
-            ? <>
+          {user ? (
+            <>
               <Link
                 to="/pokedex"
                 className="bg-blue-400 hover:bg-blue-500 text-center text-black font-bold py-2 px-4 rounded-lg shadow-lg hover:shadow-md transition-all"
@@ -38,7 +40,8 @@ const Home = ({ user, appName }) => {
                 Random fight
               </Link>
             </>
-            : <>
+          ) : (
+            <>
               <Link
                 to="/create-account"
                 className="bg-green-500 hover:bg-green-400 text-center text-black font-bold py-2 px-4 rounded-lg shadow-lg hover:shadow-md transition-all"
@@ -52,7 +55,7 @@ const Home = ({ user, appName }) => {
                 Login
               </Link>
             </>
-          }
+          )}
         </div>
       </div>
     </main>
